caa6bcb272dddf16b0da37eddc6812e3ee30193b
liortlv <99750449+liortlv@users.noreply.github.com>
2022-07-21 16:46:26 +0300

    Add env file

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+require("dotenv").config();
 // const api = require("./src/routes/itemRouter");
+
 const main = async () => {
    const app = express();
    var cors = require("cors");
 
-   app.use(cors()); // Use this after the variable declaration
-   app.use(bodyParser.json());
-   app.use(bodyParser.urlencoded({ extended: false }));
+   app.use(cors(), bodyParser.json(), bodyParser.urlencoded({ extended: false }));
    // app.use("/", api);
-   app.use(express.static(path.resolve(__dirname, "../client/build")));
+   // app.use(express.static(path.resolve(__dirname, "../client/build")));
+
    const port = process.env.PORT || "8000";
    app.listen(port, function () {
       console.log("Running on " + port);
