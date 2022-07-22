0dc9a7b06bafca9c0fb83f0d088be70e7be1cb1a
liortlv <99750449+liortlv@users.noreply.github.com>
2022-07-22 17:26:51 +0300

    Add createUser function and UserManager class

--- server/src/services/userSerivce.js
+++ server/src/services/userSerivce.js
@@ -1,1 +1,11 @@
 const { User } = require("../../storage/models/user");
+
+class UserManager {
+   async createUser(user) {
+      const response = await User.create(user);
+
+      return response;
+   }
+}
+
+module.exports = new UserManager();
