571180c8d5e64d58b1a202840241d0894cec08c2
yfrach1 <yfrach1@gmail.com>
2022-07-23 18:04:51 +0300

    home page 80% done

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@
 //import NavigationBar from "./components/NavigationBar/NavigationBar";
 //import TodoSwitch from "./navigation/TodoSwitch";
 import React from "react";
-import HomePage from "./components/homePage/HomePage";
+import HomePage from "./components/guest/MainPage/Main";
+import GuestSwitch from "./navigation/GuestSwitch";
 import styles from "./App.module.css";
 function App() {
   return (
     <div>
       {/* landingPage */}
-      <HomePage />
+      <GuestSwitch />
     </div>
 
     // <ChakraProvider>
