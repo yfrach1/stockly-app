1cc177bf7df032c19edce8fbde5957ee2b3ac9c6
liortlv <99750449+liortlv@users.noreply.github.com>
2022-07-23 16:52:43 +0300

    Add sidebar with routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
-import styles from "./App.module.css";
-import NavigationBar from "./components/NavigationBar/NavigationBar";
-import TodoSwitch from "./navigation/TodoSwitch";
+// import styles from "./App.module.css";
+import Sidebar from "./components/Sidebar/Sidebar";
 import React from "react";
+import Routes from "./pages/_routes";
+
 function App() {
-  return <div></div>;
+   return (
+      <div>
+         <Routes />
+         <Sidebar />
+      </div>
+   );
 }
 
 export default App;
