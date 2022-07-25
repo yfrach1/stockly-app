b0e26ab9c6063831fe475cad6db3e1a61d2dbf6e
liortlv <99750449+liortlv@users.noreply.github.com>
2022-07-25 15:08:20 +0300

    Add JWT auth

--- server/src/controllers/userContorller.js
+++ server/src/controllers/userContorller.js
@@ -1,20 +1,28 @@
 const userService = require("../services/userService");
 
 async function createUser(req, res) {
-  const response = await userService.createUser(req.body);
-  response
-    ? res.status(200).json(req.body)
-    : res.status(400).json({ error: "User already exist" });
+   const response = await userService.createUser(req.body);
+   response
+      ? res.status(200).json(req.body)
+      : res.status(400).json({ error: "User already exist" });
 }
 
 async function loginUser(req, res) {
-  const userData = await userService.loginUser(req.body);
-  userData
-    ? res.status(200).json(userData)
-    : res.status(400).json({ error: "Username or password is wrong" });
+   const userData = await userService.loginUser(req.body);
+   userData
+      ? res.status(200).json(userData)
+      : res.status(400).json({ error: "Username or password is wrong" });
+}
+
+async function getUserData(req, res) {
+   const userData = await userService.getUserData(req.user);
+   userData
+      ? res.status(200).json(userData)
+      : res.status(401).json({ error: "Authorization denied" });
 }
 
 module.exports = {
-  createUser,
-  loginUser,
+   createUser,
+   loginUser,
+   getUserData,
 };
--- server/src/routes/userRouter.js
+++ server/src/routes/userRouter.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const userRouter = express.Router();
-const { createUser, loginUser } = require("../controllers/userContorller");
+const { createUser, loginUser, getUserData } = require("../controllers/userContorller");
+const authenticateToken = require("../middlewares/authenticateToken");
 
-userRouter.post("/", createUser);
-userRouter.get("/", loginUser);
+userRouter.post("/signUp", createUser);
+userRouter.post("/signIn", loginUser);
+userRouter.get("/", authenticateToken, getUserData);
 
 module.exports = userRouter;
