e48e446f5a688b0125cac0612f4c03745a49f997
liortlv <99750449+liortlv@users.noreply.github.com>
2022-07-26 16:35:25 +0300

    Add user info to responses

--- server/src/controllers/userContorller.js
+++ server/src/controllers/userContorller.js
@@ -1,28 +1,22 @@
 const userService = require("../services/userService");
 
 async function createUser(req, res) {
-   const accessToken = await userService.createUser(req.body);
+   const { accessToken, user, portfolio } = await userService.createUser(req.body);
    accessToken
-      ? res
-           .status(200)
-           .cookie("token", accessToken, { httpOnly: true })
-           .json({ message: "User created successfuly" })
+      ? res.status(200).cookie("token", accessToken, { httpOnly: true }).json({ user, portfolio })
       : res.status(200).json({ error: "User already exist" });
 }
 
 async function loginUser(req, res) {
-   const accessToken = await userService.loginUser(req.body);
+   const { accessToken, allUserData } = await userService.loginUser(req.body);
 
    accessToken
-      ? res
-           .status(200)
-           .cookie("token", accessToken, { httpOnly: true })
-           .json({ message: "Login successful" })
+      ? res.status(200).cookie("token", accessToken, { httpOnly: true }).json(allUserData)
       : res.status(200).json({ error: "Username or password is wrong" });
 }
 
 async function getUserData(req, res) {
-   const userData = await userService.getUserData(req.user);
+   const userData = await userService.getUserData(req.user.id);
    userData
       ? res.status(200).json(userData)
       : res.status(401).json({ error: "Authorization denied" });
