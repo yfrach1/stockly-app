cd9802aebcc56d9e954430422c43f88df61eeb79
liortlv <99750449+liortlv@users.noreply.github.com>
2022-07-29 00:30:04 +0300

    Fix api search terms bug
    Some search terms would return undefined in some of their results. Fixed it with if exist

--- server/src/services/stockService.js
+++ server/src/services/stockService.js
@@ -14,9 +14,6 @@ class StockManager {
       return stocks;
    }
    async addStock(stock, user) {
-      //  console.log("stockSearchKeyddd: ", stockSearchKey);
-      // stockClient.searchStock(stockSearchKey);
-
       const UserPortfolio = await Portfolio.findOne({
          where: { user_id: user.id },
       });
@@ -39,16 +36,16 @@ class StockManager {
 
    async searchStock(stockSearchKey, portfolioId) {
       const stocksDetailsFromDB = await this._searchStockInDB(portfolioId, stockSearchKey);
-      console.log("stocksDetailsFromDB: ", stocksDetailsFromDB);
+      // console.log("stocksDetailsFromDB: ", stocksDetailsFromDB);
       const stocksDetailsFromApi = await stockClient.searchStock(stockSearchKey, portfolioId);
 
-      console.log("search result: ", stocksDetailsFromApi);
+      // console.log("search result: ", stocksDetailsFromApi);
 
       const searchResult = stocksDetailsFromApi.map((result) => {
          result.isMine = this.doesTickerExistInDb(result.ticker, stocksDetailsFromDB);
          return result;
       });
-      console.log("searchResult: ", searchResult);
+      // console.log("searchResult: ", searchResult);
 
       return searchResult;
    }
