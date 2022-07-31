97fdc77b7b0d589aca365b75af449b8b832109a6
liortlv <99750449+liortlv@users.noreply.github.com>
2022-07-31 18:33:36 +0300

    Add stock quantity add button integration

--- client/src/app/actions/user-entities-actions.js
+++ client/src/app/actions/user-entities-actions.js
@@ -1,189 +1,179 @@
 import actionsTypes from "./constants";
-import {
-  addNewUser,
-  validateUser,
-  getUserDataOnStart,
-  logOutUser,
-} from "../services/userService";
-import {
-  addStock,
-  getStockDetails,
-  searchStock,
-  deleteStock,
-} from "../services/stockService";
+import { addNewUser, validateUser, getUserDataOnStart, logOutUser } from "../services/userService";
+import { addStock, getStockDetails, searchStock, deleteStock } from "../services/stockService";
 
 const signUpRequest = () => ({
-  type: actionsTypes.SIGN_UP_REQUEST,
+   type: actionsTypes.SIGN_UP_REQUEST,
 });
 
 const signUpRequestSuccessed = (userData) => ({
-  type: actionsTypes.SIGN_UP_REQUEST_SUCCESSED,
-  userData,
+   type: actionsTypes.SIGN_UP_REQUEST_SUCCESSED,
+   userData,
 });
 const signUpRequestFailed = () => ({
-  type: actionsTypes.SIGN_UP_REQUEST_FAILED,
+   type: actionsTypes.SIGN_UP_REQUEST_FAILED,
 });
 
 const signInRequest = () => ({
-  type: actionsTypes.SIGN_IN_REQUEST,
+   type: actionsTypes.SIGN_IN_REQUEST,
 });
 const signInRequestSuccessed = (userData) => ({
-  type: actionsTypes.SIGN_IN_REQUEST_SUCCESSED,
-  userData,
+   type: actionsTypes.SIGN_IN_REQUEST_SUCCESSED,
+   userData,
 });
 
 const signOutRequestSuccessed = (userData) => ({
-  type: actionsTypes.SIGN_OUT_REQUEST_SUCCESSED,
+   type: actionsTypes.SIGN_OUT_REQUEST_SUCCESSED,
 });
 
 const checkTokenRequest = () => ({
-  type: actionsTypes.CHECK_USER_TOKEN_REQUEST,
+   type: actionsTypes.CHECK_USER_TOKEN_REQUEST,
 });
 
 const checkTokenRequestSuccessed = (userData) => ({
-  type: actionsTypes.CHECK_USER_TOKEN_REQUEST_SUCCESSED,
-  userData,
+   type: actionsTypes.CHECK_USER_TOKEN_REQUEST_SUCCESSED,
+   userData,
 });
 
 const checkTokenRequestFailed = (userData) => ({
-  type: actionsTypes.CHECK_USER_TOKEN_REQUEST_FAILED,
-  userData,
+   type: actionsTypes.CHECK_USER_TOKEN_REQUEST_FAILED,
+   userData,
 });
 
 const searchStockRequest = (stocks) => ({
-  type: actionsTypes.SEARCH_STOCK_REQUEST,
-  stocks,
+   type: actionsTypes.SEARCH_STOCK_REQUEST,
+   stocks,
 });
 
 const searchStockSuccessed = (stocks) => ({
-  type: actionsTypes.SEARCH_STOCK_REQUEST_SUCCESSED,
-  stocks,
+   type: actionsTypes.SEARCH_STOCK_REQUEST_SUCCESSED,
+   stocks,
 });
 
 const searchStockFailed = (stocks) => ({
-  type: actionsTypes.SEARCH_STOCK_REQUEST_FAILED,
-  stocks,
+   type: actionsTypes.SEARCH_STOCK_REQUEST_FAILED,
+   stocks,
 });
 
 const addStockSuccessed = (stockTicker) => ({
-  type: actionsTypes.ADD_STOCK_REQUEST_SUCCESSED,
-  stockTicker,
+   type: actionsTypes.ADD_STOCK_REQUEST_SUCCESSED,
+   stockTicker,
 });
 
 const deleteStockSuccessed = (stockTicker) => ({
-  type: actionsTypes.DELETE_STOCK_REQUEST_SUCCESSED,
-  stockTicker,
+   type: actionsTypes.DELETE_STOCK_REQUEST_SUCCESSED,
+   stockTicker,
 });
 
 const getStockDetailsSuccessed = (stockData, stock) => ({
-  type: actionsTypes.GET_STOCK_DETAILS_REQUEST_SUCCESSED,
-  payload: { stockData, stock },
+   type: actionsTypes.GET_STOCK_DETAILS_REQUEST_SUCCESSED,
+   payload: { stockData, stock },
 });
 
 export const signUpAction = (newUserData) => {
-  return async (dispatch) => {
-    dispatch(signUpRequest());
-    try {
-      const res = await addNewUser(newUserData);
-      if (res.data) {
-        dispatch(signUpRequestSuccessed(res.data));
-      } else {
-        dispatch(signUpRequestFailed(res.data.message));
+   return async (dispatch) => {
+      dispatch(signUpRequest());
+      try {
+         const res = await addNewUser(newUserData);
+         if (res.data) {
+            dispatch(signUpRequestSuccessed(res.data));
+         } else {
+            dispatch(signUpRequestFailed(res.data.message));
+         }
+      } catch (error) {
+         // const stopLoadin = setTimeout(() => {
+         //   dispatch(signUpRequestFailed("error"));
+         // }, 1000);
+         // stopLoadin();
+         dispatch(signUpRequestFailed("error"));
       }
-    } catch (error) {
-      // const stopLoadin = setTimeout(() => {
-      //   dispatch(signUpRequestFailed("error"));
-      // }, 1000);
-      // stopLoadin();
-      dispatch(signUpRequestFailed("error"));
-    }
-  };
+   };
 };
 
 export const signInAction = (loginUserData) => {
-  return async (dispatch) => {
-    dispatch(signInRequest());
-    try {
-      const res = await validateUser(loginUserData);
-      dispatch(signInRequestSuccessed(res.data));
-    } catch (error) {
-      console.log(error.message);
-      //dispatch(signUpRequestFailed());
-    }
-  };
+   return async (dispatch) => {
+      dispatch(signInRequest());
+      try {
+         const res = await validateUser(loginUserData);
+         dispatch(signInRequestSuccessed(res.data));
+      } catch (error) {
+         console.log(error.message);
+         //dispatch(signUpRequestFailed());
+      }
+   };
 };
 
 export const signOutAction = () => {
-  return async (dispatch) => {
-    // dispatch(signInRequest());
-    try {
-      const res = await logOutUser();
-      dispatch(signOutRequestSuccessed(res));
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
+   return async (dispatch) => {
+      // dispatch(signInRequest());
+      try {
+         const res = await logOutUser();
+         dispatch(signOutRequestSuccessed(res));
+      } catch (error) {
+         console.log(error.message);
+      }
+   };
 };
 
 export const checkUserTokenAction = () => {
-  return async (dispatch) => {
-    //dispatch loader maybe
-    dispatch(checkTokenRequest());
-    try {
-      const res = await getUserDataOnStart();
-      console.log(res);
-      dispatch(checkTokenRequestSuccessed(res.data));
-    } catch (error) {
-      console.log(error);
-      dispatch(checkTokenRequestFailed());
-    }
-  };
+   return async (dispatch) => {
+      //dispatch loader maybe
+      dispatch(checkTokenRequest());
+      try {
+         const res = await getUserDataOnStart();
+         console.log(res);
+         dispatch(checkTokenRequestSuccessed(res.data));
+      } catch (error) {
+         console.log(error);
+         dispatch(checkTokenRequestFailed());
+      }
+   };
 };
 
-export const addStockAction = (stock) => {
-  return async (dispatch) => {
-    //dispatch loader maybe
-    try {
-      await addStock(stock);
-      dispatch(addStockSuccessed(stock.ticker));
-    } catch (error) {}
-  };
+export const addStockAction = (stock, quantity) => {
+   return async (dispatch) => {
+      //dispatch loader maybe
+      try {
+         await addStock(stock);
+         dispatch(addStockSuccessed(stock.ticker));
+      } catch (error) {}
+   };
 };
 
 export const deleteStockAction = (stockTicker, stockId) => {
-  return async (dispatch) => {
-    try {
-      const res = await deleteStock(stockId);
-      if (res) dispatch(deleteStockSuccessed(stockTicker));
-    } catch (error) {}
-  };
+   return async (dispatch) => {
+      try {
+         const res = await deleteStock(stockId);
+         if (res) dispatch(deleteStockSuccessed(stockTicker));
+      } catch (error) {}
+   };
 };
 
 export const searchStockAction = (stockSearchKey, portfolioId) => {
-  return async (dispatch) => {
-    dispatch(searchStockRequest());
-    try {
-      const res = await searchStock(stockSearchKey, portfolioId);
-      dispatch(searchStockSuccessed(res.data));
-    } catch (error) {
-      console.log("error: ", error);
-      dispatch(searchStockFailed());
-    }
-  };
+   return async (dispatch) => {
+      dispatch(searchStockRequest());
+      try {
+         const res = await searchStock(stockSearchKey, portfolioId);
+         dispatch(searchStockSuccessed(res.data));
+      } catch (error) {
+         console.log("error: ", error);
+         dispatch(searchStockFailed());
+      }
+   };
 };
 
 export const getStockDetailsAction = (stock) => {
-  return async (dispatch) => {
-    //dispatch loader maybe
-    try {
-      console.log("stock:", stock);
-      const stockData = await getStockDetails(stock.ticker);
+   return async (dispatch) => {
+      //dispatch loader maybe
+      try {
+         console.log("stock:", stock);
+         const stockData = await getStockDetails(stock.ticker);
 
-      dispatch(getStockDetailsSuccessed({ stockData, stock }));
-    } catch (error) {
-      console.log(error);
-    }
-  };
+         dispatch(getStockDetailsSuccessed({ stockData, stock }));
+      } catch (error) {
+         console.log(error);
+      }
+   };
 };
 
 // export const updateStockAction = (stock) => {
--- client/src/components/user/stocksSystem/stockDetails/StockDetails.jsx
+++ client/src/components/user/stocksSystem/stockDetails/StockDetails.jsx
@@ -1,53 +1,59 @@
 import React from "react";
+import { useState, useCallback, useRef } from "react";
 import StockGraph from "../stockGraph/StockGraph";
 import styles from "./StockDetails.module.css";
 
-const StockDetails = ({
-  stock,
-  stockInfo,
-  deleteStockAction,
-  addStockAction,
-}) => {
-  console.log("stock: ", stock);
-  console.log("stockInfo: ", stockInfo);
+const StockDetails = ({ stock, stockInfo, deleteStockAction, addStockAction }) => {
+   const [quantity, setQuantity] = useState(0);
+   const inputElement = useRef(null);
 
-  return (
-    <>
-      <div className={styles.stockDetailsContainer}>
-        <h1>{stock.ticker}</h1>
-        <h2>{stock.name}</h2>
-        <StockGraph stockInfo={stockInfo} />
-        <div className={styles.details}>
-          <span>
-            Open: {stockInfo.data ? `${stockInfo.data.at(-1).open}` : ""}
-          </span>
-          <span>
-            High: {stockInfo.data ? `${stockInfo.data.at(-1).high}` : ""}
-          </span>
-          <span>
-            Low: {stockInfo.data ? `${stockInfo.data.at(-1).low}` : ""}
-          </span>
-          <span>
-            Volume:{" "}
-            {stockInfo.data ? `${stockInfo.data.at(-1).volume / 1000}K` : ""}
-          </span>
-        </div>
+   const handleChange = useCallback(
+      (e) => {
+         setQuantity(e.target.value);
+      },
+      [setQuantity]
+   );
 
-        <input type="text" />
-        <button
-          onClick={() => (stock.isMine ? () => {} : addStockAction(stock))}
-          className={styles.buttonDelete}
-        >
-          {stock.isMine ? "Update" : "Add"}
-        </button>
-        <button
-          onClick={() => deleteStockAction(stock.ticker, stock.stock_id)}
-          className={styles.buttonDelete}
-        >
-          Delete
-        </button>
-      </div>
-    </>
-  );
+   const addStock = useCallback(() => {
+      stock.quantity = quantity;
+      addStockAction(stock);
+      inputElement.current.value = "";
+   }, [quantity]);
+   return (
+      <>
+         <div className={styles.stockDetailsContainer}>
+            <h1>{stock.ticker}</h1>
+            <h2>{stock.name}</h2>
+            <StockGraph stockInfo={stockInfo} />
+            <div className={styles.details}>
+               <span>Open: {stockInfo.data ? `${stockInfo.data.at(-1).open}` : ""}</span>
+               <span>High: {stockInfo.data ? `${stockInfo.data.at(-1).high}` : ""}</span>
+               <span>Low: {stockInfo.data ? `${stockInfo.data.at(-1).low}` : ""}</span>
+               <span>
+                  Volume: {stockInfo.data ? `${stockInfo.data.at(-1).volume / 1000}K` : ""}
+               </span>
+            </div>
+
+            <input
+               type="text"
+               placeholder={stock.quantity ? stock.quantity : ""}
+               onChange={handleChange}
+               ref={inputElement}
+            />
+            <button
+               onClick={() => (stock.isMine ? "" : addStock())}
+               className={styles.buttonDelete}
+            >
+               {stock.isMine ? "Update" : "Add"}
+            </button>
+            <button
+               onClick={() => deleteStockAction(stock.ticker, stock.stock_id)}
+               className={styles.buttonDelete}
+            >
+               Delete
+            </button>
+         </div>
+      </>
+   );
 };
 export default StockDetails;
