04a712e432a2ec82be7ef4c6abaa176260f587f9
liortlv <99750449+liortlv@users.noreply.github.com>
2022-08-01 01:01:21 +0300

    Refactor seperating line desin

--- client/src/components/user/stocks/ListStocks.jsx
+++ client/src/components/user/stocks/ListStocks.jsx
@@ -3,28 +3,26 @@ import { v4 as uuidv4 } from "uuid";
 import StockCardConnector from "./stockCard/StockCardConnector";
 
 const ListStocks = ({ stocks }) => {
-  console.log("stocks: ", stocks);
-  const notMyStocksArray = stocks
-    .map((stock, index) => {
-      return stock.isMine ? null : (
-        <StockCardConnector key={index} stock={stock} />
-      );
-    })
-    .filter((stock) => stock !== null);
-  //   console.log("notMyStocksArray: ", notMyStocksArray.length);
-  //   console.log("notMyStocksArray: ", notMyStocksArray);
-  const sepreatedLine = "********************************";
-  return (
-    <div>
-      <div className={styles.grid}>
-        {stocks.map((stock, index) =>
-          stock.isMine ? <StockCardConnector key={index} stock={stock} /> : null
-        )}
-        {notMyStocksArray.length ? sepreatedLine : null}
-        {notMyStocksArray}
+   console.log("stocks: ", stocks);
+   const notMyStocksArray = stocks
+      .map((stock, index) => {
+         return stock.isMine ? null : <StockCardConnector key={index} stock={stock} />;
+      })
+      .filter((stock) => stock !== null);
+   //   console.log("notMyStocksArray: ", notMyStocksArray.length);
+   //   console.log("notMyStocksArray: ", notMyStocksArray);
+   const seperatingLine = <span className={styles.seperatingLine}></span>;
+   return (
+      <div>
+         <div className={styles.grid}>
+            {stocks.map((stock, index) =>
+               stock.isMine ? <StockCardConnector key={index} stock={stock} /> : null
+            )}
+            {notMyStocksArray.length ? seperatingLine : null}
+            {notMyStocksArray}
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default ListStocks;
--- client/src/components/user/stocks/stockCard/StockCard.jsx
+++ client/src/components/user/stocks/stockCard/StockCard.jsx
@@ -1,35 +1,36 @@
 import React from "react";
 import styles from "./StockCard.module.css";
 
-const StockCard = ({ stock, getStockDetailsAction  ,getStockNewsAction}) => {
-  return (
-    <div
-      className={styles.stockCard}
-      onClick={() => {getStockDetailsAction(stock) ;getStockNewsAction(stock)}}
-    >
-      <div className={styles.alignStockRow}>
-        <div className={styles.ticker}>{stock.ticker}</div>
-        <div className={styles.price}>{stock.price}</div>
-      </div>
+const StockCard = ({ stock, getStockDetailsAction, getStockNewsAction }) => {
+   return (
+      <div
+         className={styles.stockCard}
+         onClick={() => {
+            getStockDetailsAction(stock);
+            getStockNewsAction(stock);
+         }}
+      >
+         <div className={styles.alignStockRow}>
+            <div className={styles.ticker}>{stock.ticker}</div>
+            <div className={styles.price}>{stock.price}</div>
+         </div>
 
-      <div className={styles.alignStockRow}>
-        <div
-          className={styles.name}
-          style={{ color: !stock.name ? "red" : "" }}
-        >
-          {stock.name ? stock.name : "name not avialbe"}
-        </div>
-        <div
-          className={styles.changePercent}
-          style={{
-            backgroundColor: stock.change_percent > 0 ? "#38ef7d" : "#F00000",
-          }}
-        >
-          {`${stock.change_percent}%`}
-        </div>
+         <div className={styles.alignStockRow}>
+            <div className={styles.name} style={{ color: !stock.name ? "red" : "" }}>
+               {stock.name ? stock.name : "name not avialbe"}
+            </div>
+            <div
+               className={styles.changePercent}
+               style={{
+                  backgroundColor: stock.change_percent > 0 ? "#38ef7d" : "#F00000",
+                  color: stock.change_percent > 0 ? "#010b13" : "#ffffff",
+               }}
+            >
+               {`${stock.change_percent}%`}
+            </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default StockCard;
