56df4c73614e5ea936838fbb8acad65d8e36a8d0
liortlv <99750449+liortlv@users.noreply.github.com>
2022-07-31 16:45:26 +0300

    Modify guest user design and date

--- client/src/components/guest/about/AboutUs.jsx
+++ client/src/components/guest/about/AboutUs.jsx
@@ -1,5 +1,37 @@
+import React from "react";
+import styles from "./AboutUs.module.css";
 const AboutUs = () => {
-  return <div>About us page</div>;
+   return (
+      <>
+         <div className={styles.title}>About Us</div>
+         <div className={styles.text}>
+            Stockly was born in 2022 as part of the Monday-U Full Stack Academy.
+         </div>
+
+         <div className={styles.creatorsTitle}>Stockly creators:</div>
+         <a
+            href="https://www.linkedin.com/in/harelyfrach"
+            target="_blank"
+            className={styles.creatorLink}
+         >
+            Harel Yfrach
+         </a>
+         <a
+            href="https://www.linkedin.com/in/lior-tal-28783224/"
+            target="_blank"
+            className={styles.creatorLink}
+         >
+            Lior Tal
+         </a>
+         <a
+            href="https://www.linkedin.com/in/shirlybarazsky/"
+            target="_blank"
+            className={styles.creatorLink}
+         >
+            Shirly Barazsky
+         </a>
+      </>
+   );
 };
 
 export default AboutUs;
