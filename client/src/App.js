9e3513ec6ea8207f06775e79bc0ab9d2b5ab27ae
liortlv <99750449+liortlv@users.noreply.github.com>
2022-08-01 15:47:33 +0300

    Refactor news card css design

--- client/src/App.js
+++ client/src/App.js
@@ -5,33 +5,27 @@ import { getData } from "../../client/src/app/services/userService";
 import UserView from "./components/user/userView/UserView";
 import PackmanLoader from "./components/loading/packmanLoader/PackmanLoader";
 import OpacityLoader from "./components/loading/fetchStockLoader/OpacityLoader";
-function App({
-  userAuth,
-  redirectLoading,
-  checkUserTokenAction,
-  showToast,
-  toastParam,
-}) {
-  // console.log("userAuth: ", userAuth);
-  // console.log("redirectLoading: ", redirectLoading);
+function App({ userAuth, redirectLoading, checkUserTokenAction, showToast, toastParam }) {
+   // console.log("userAuth: ", userAuth);
+   // console.log("redirectLoading: ", redirectLoading);
 
-  useEffect(() => {
-    console.log("in use effect");
-    checkUserTokenAction();
-  }, []);
-  return (
-    <>
-      <MyToast
+   useEffect(() => {
+      console.log("in use effect");
+      checkUserTokenAction();
+   }, []);
+   return (
+      <>
+         {/* <MyToast
         showToast={showToast}
         property={toastParam}
         hideToastAction={hideToastAction}
-      />
-      {redirectLoading ? (
-        <PackmanLoader />
-      ) : (
-        <div className={styles.app}>{userAuth ? <UserView /> : <Main />}</div>
-      )}
-    </>
-  );
+      /> */}
+         {redirectLoading ? (
+            <PackmanLoader />
+         ) : (
+            <div className={styles.app}>{userAuth ? <UserView /> : <Main />}</div>
+         )}
+      </>
+   );
 }
 export default App;
