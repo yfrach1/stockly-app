27331e24bb228e6712f9e6e28f84949a349c2a89
liortlv <99750449+liortlv@users.noreply.github.com>
2022-08-02 00:37:22 +0300

    Fix login bug

--- server/src/routes/portfolioRouter.js
+++ server/src/routes/portfolioRouter.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const portfolioRouter = express.Router();
-const { createPortfolio } = require("../controllers/portfolioContorller");
+const {
+   createPortfolio,
+   getPortfolioPerformanceData,
+} = require("../controllers/portfolioContorller");
+const authenticateToken = require("../middlewares/authenticateToken");
 
 portfolioRouter.post("/", createPortfolio);
+portfolioRouter.post("/performance", authenticateToken, getPortfolioPerformanceData);
 
 module.exports = portfolioRouter;
