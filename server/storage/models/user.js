4bf6754cbcabbcfd21d9a08a40ba07e486065cf9
liortlv <99750449+liortlv@users.noreply.github.com>
2022-08-01 19:54:40 +0300

    Update user table to have many portfolio

--- server/storage/models/user.js
+++ server/storage/models/user.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
        * The `models/index` file will call this method automatically.
        */
       static associate(models) {
-         User.hasOne(models.Portfolio, { foreignKey: "user_id" });
+         User.hasMany(models.Portfolio, { foreignKey: "user_id" });
       }
    }
    User.init(
