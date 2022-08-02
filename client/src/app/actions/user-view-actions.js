c0804a80aafcbe13d27f8f183abbcd792137f0c3
liortlv <99750449+liortlv@users.noreply.github.com>
2022-08-02 16:45:48 +0300

    Add gathering and calculation of portfolio history data at Server

--- client/src/app/actions/user-view-actions.js
+++ client/src/app/actions/user-view-actions.js
@@ -1,39 +1,35 @@
 import actionsTypes from "./constants";
-import {
-  getStockDetails,
-  getPortfolioDetails,
-  getStockNews,
-} from "../services/stockService";
+import { getStockDetails, getPortfolioDetails, getStockNews } from "../services/stockService";
 
 const getStockNewsSuccessed = (stockNews) => ({
-  type: actionsTypes.GET_STOCK_NEWS_REQUEST_SUCCESSED,
-  payload: { stockNews },
+   type: actionsTypes.GET_STOCK_NEWS_REQUEST_SUCCESSED,
+   payload: { stockNews },
 });
 
 const hideToast = () => ({
-  type: actionsTypes.HIDE_TOAST,
+   type: actionsTypes.HIDE_TOAST,
 });
 
 export const hideToastAction = () => {
-  return (dispatch) => dispatch(hideToast());
+   return (dispatch) => dispatch(hideToast());
 };
 
-export const getPortfolioHistorysAction = (stocks) => {
-  return async (dispatch) => {
-    try {
-      const stockData = await getPortfolioDetails(stocks);
-      //need to return and dispatch
-    } catch (error) {}
-  };
-};
+// export const getPortfolioHistorysAction = (stocks) => {
+//   return async (dispatch) => {
+//     try {
+//       const stockData = await getPortfolioDetails(stocks);
+//       //need to return and dispatch
+//     } catch (error) {}
+//   };
+// };
 
 export const getStockNewsAction = (stock) => {
-  return async (dispatch) => {
-    try {
-      const stockNews = await getStockNews(stock.ticker);
-      dispatch(getStockNewsSuccessed(stockNews));
-    } catch (error) {
-      console.log(error);
-    }
-  };
+   return async (dispatch) => {
+      try {
+         const stockNews = await getStockNews(stock.ticker);
+         dispatch(getStockNewsSuccessed(stockNews));
+      } catch (error) {
+         console.log(error);
+      }
+   };
 };
