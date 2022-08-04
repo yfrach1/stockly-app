d4b5fc09e07b87f4c7129da3d3db45120abb0135
liortlv <99750449+liortlv@users.noreply.github.com>
2022-08-04 16:53:45 +0300

    Modify empty state image and text

--- client/src/components/user/stocks/listStocks/ListStocks.jsx
+++ client/src/components/user/stocks/listStocks/ListStocks.jsx
@@ -1,6 +1,6 @@
 import styles from "./ListStocks.module.css";
 import StockCardConnector from "../stockCard/StockCardConnector";
-import emptyState from "../../../../assets/images/emptyState.png";
+import emptyStateImage from "../../../../assets/images/emptyState.png";
 
 const ListStocks = ({ stocks }) => {
   console.log("stocks: ", stocks);
@@ -12,13 +12,19 @@ const ListStocks = ({ stocks }) => {
     })
     .filter((stock) => stock !== null);
   const seperatingLine = <span className={styles.seperatingLine}></span>;
+  const emptyState = (
+    <>
+      <img
+        src={emptyStateImage}
+        alt="Empty state"
+        className={styles.emptyState}
+      />
+      <div className={styles.addText}>Add your first stock up here!</div>
+    </>
+  );
   return (
     <div className={styles.grid}>
-      {!stocks.length ? (
-        <img src={emptyState} alt="Empty state" className={styles.emptyState} />
-      ) : (
-        <></>
-      )}
+      {!stocks.length ? emptyState : <></>}
       {stocks.map((stock, index) =>
         stock.isMine ? <StockCardConnector key={index} stock={stock} /> : null
       )}
