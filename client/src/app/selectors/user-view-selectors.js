6c07fac2416d35584840c10d2959fe9130a73af9
Harel Yfrach <34807437+yfrach1@users.noreply.github.com>
2022-08-04 15:26:08 +0300

    fix create selector

--- client/src/app/selectors/user-view-selectors.js
+++ client/src/app/selectors/user-view-selectors.js
@@ -1,3 +1,8 @@
+import { createSelector } from "@reduxjs/toolkit";
+import {
+  getStockRevenue,
+  getStockDiffPercent,
+} from "./user-entities-selectors";
 const getUserView = (state) => state.userView;
 export const getUserEntities = (state) => state.userEntities;
 
@@ -10,24 +15,34 @@ export const getShowToast = (state) => getUserView(state).showToast;
 export const getToastParam = (state) => getUserView(state).toastParam;
 
 // export const getFilter = (state) => getUserView(state).dateFilter;
-export const getTimePeriodFilter = (state) => getUserView(state).dateFilter;
+export const getDateFilter = (state) => getUserView(state).dateFilter;
 
-export const getRevenue = (state) => {
-  let dateFilter = getUserView(state).dateFilter;
-  if (dateFilter === "All") {
-    dateFilter = "1All";
-  }
-  const revenueObj = getUserEntities(state).stockDetails.stockRevenue;
+export const getRevenue = createSelector(
+  [getStockRevenue, getDateFilter],
+  (revenue, filter) => revenue[filter]
+);
 
-  return revenueObj[dateFilter];
-};
+export const getDiffPercent = createSelector(
+  [getStockDiffPercent, getDateFilter],
+  (DiffPercent, filter) => DiffPercent[filter]
+);
 
-export const getDiffPercent = (state) => {
-  let dateFilter = getUserView(state).dateFilter;
-  if (dateFilter === "All") {
-    dateFilter = "1All";
-  }
-  const diffPercentObj = getUserEntities(state).stockDetails.stockDiffPercent;
+// export const getDiffPercent = (state) => {
+//   let dateFilter = getUserView(state).dateFilter;
+//   if (dateFilter === "All") {
+//     dateFilter = "All";
+//   }
+//   const diffPercentObj = getUserEntities(state).stockDetails.stockDiffPercent;
 
-  return diffPercentObj[dateFilter];
-};
+//   return diffPercentObj[dateFilter];
+// };
+
+// export const getRevenue = (state) => {
+//   let dateFilter = getUserView(state).dateFilter;
+//   if (dateFilter === "All") {
+//     dateFilter = "1All";
+//   }
+//   const revenueObj = getUserEntities(state).stockDetails.stockRevenue;
+
+//   return revenueObj[dateFilter];
+// };
