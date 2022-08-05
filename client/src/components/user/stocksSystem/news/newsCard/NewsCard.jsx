f8ec5cecdb8cc87562f9645172ff75961d43226f
liortlv <99750449+liortlv@users.noreply.github.com>
2022-08-05 11:16:42 +0300

    Modify news flex order

--- client/src/components/user/stocksSystem/news/newsCard/NewsCard.jsx
+++ client/src/components/user/stocksSystem/news/newsCard/NewsCard.jsx
@@ -2,16 +2,23 @@ import React from "react";
 import styles from "./NewsCard.module.css";
 
 const NewsCard = ({ title, publishedDate, description, url, source }) => {
-   return (
-      <a href={url} target="_blank" rel="noopener noreferrer" className={styles.newsCard}>
-         <div className={styles.title}>{title}</div>
-         <div className={styles.description}>{description}</div>
-         <div className={styles.footer}>
-            <div>{source}</div>
-            <div>{publishedDate.split("T")[0]}</div>
-         </div>
-      </a>
-   );
+  return (
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={styles.newsCard}
+    >
+      <div className={styles.title}>{title}</div>
+      <div className={styles.footer}>
+        <div className={styles.description}>{description}</div>
+        <div>
+          <div>{source}</div>
+          <div>{publishedDate.split("T")[0]}</div>
+        </div>
+      </div>
+    </a>
+  );
 };
 
 export default NewsCard;
