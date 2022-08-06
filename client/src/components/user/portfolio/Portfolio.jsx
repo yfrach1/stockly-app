fa82a242ac2b33800dfeea997f39a54c2752e5b1
liortlv <99750449+liortlv@users.noreply.github.com>
2022-08-06 20:52:31 +0300

    Redesign loader

--- client/src/components/user/portfolio/Portfolio.jsx
+++ client/src/components/user/portfolio/Portfolio.jsx
@@ -3,18 +3,22 @@ import styles from "./Portfolio.module.css";
 import SearchBarConnector from "../stocksSystem/searchBar/SearchBar-connector";
 import ListStocksConnector from "../stocks/listStocks/ListStocksConnector";
 import OpacityLoader from "../../loading/fetchStockLoader/OpacityLoader";
-import PortfolioDataConnector from "./portfolioData/PortfolioDataConnector";
 import DetailsSwitchConnector from "../../../navigation/DetailsSwitchConnector";
 import PortfolioCardConnector from "./portfolioCard/PortfolioCardConnector";
 const Portfolio = ({ fetchLoading, portfolioId }) => {
   return (
     <div className={styles.portfolioGrid}>
       <div className={styles.portfolioPreview}>
-        {/* <PortfolioDataConnector /> */}
         <PortfolioCardConnector />
         <SearchBarConnector portfolioId={portfolioId} />
         <div className={styles.listStocksPreview}>
-          {fetchLoading ? <OpacityLoader /> : <ListStocksConnector />}
+          {fetchLoading ? (
+            <div className={styles.loaderContainer}>
+              <OpacityLoader />{" "}
+            </div>
+          ) : (
+            <ListStocksConnector />
+          )}
         </div>
       </div>
       <div className={styles.stockPreview}>{<DetailsSwitchConnector />}</div>
